a68aced8f266930258ae5f4d6f8b08d96dacfb3d
Pritam Sarbajna <pritamsarbajna10@gmail.com>
2023-05-12 17:56:27 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastApi
+import pickle
+import pandas as pd
+from BankNotes import BankNote
+
+app = FastApi()
+
+pickle_in = open('classifier.pkl', 'rb')
+clf = pickle.load(pickle_in)
+
+@app.get('/')
+async def index():
+    return {'msg': 'Hello world'}
+
+@app.post('/predict')
+async def predict(data:BankNote):
+    data = data.dict()
+    variance = data['variance']
+    skewness = data['skewness']
+    curtosis = data['curtosis']
+    entropy = data['entropy']
+    
+    prediction = clf.predict([[variance, skewness, curtosis, entropy]])
+    
+    return {'prediction': prediction}
