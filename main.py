c124641cb8f36e1866032d86fb1ac67ee0a6e430
Pritam Sarbajna <pritamsarbajna10@gmail.com>
2023-05-13 19:47:31 +0530

    lol

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import pickle
 import pandas as pd
 from BankNotes import BankNote
+import uvicorn
 
 app = FastAPI()
 
@@ -20,6 +21,15 @@ async def predict(data:BankNote):
     curtosis = data['curtosis']
     entropy = data['entropy']
     
-    prediction = clf.predict([[variance, skewness, curtosis, entropy]])
+    prediction = clf.predict([[variance,skewness,curtosis,entropy]])
     
-    return {'prediction': prediction}
+    if(prediction[0]>0.5):
+        prediction="Fake note"
+    else:
+        prediction="Its a Bank note"
+    return {
+        'prediction': prediction
+    }
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=10000)
